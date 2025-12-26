bc13bbc0cec2e55e598cfe19ef0dffdf4a394100
Gustav Abrahamsson <gustav.a.abrahamsson@gmail.com>
2025-12-26 17:38:41 +0100

    Revert "Implement fuzzy search by adding wild card in between all letters in search term"
    This reverts commit b3e9c93172fd791ee431e9ee64e01fcfd2f61e5f.

--- connections.sh
+++ connections.sh
@@ -6,9 +6,8 @@ connections=$(cat ~/connect/connections)
 if [ $# -gt 0 ]; then
         search="$*"
         for i in $search; do
-                # Insert .* between characters to allow for fuzzy matching
-                fuzzy_search=$(echo "$i" | sed 's/./&.*/g')
-                connections=$(echo "$connections" | grep -E -i "$fuzzy_search")
+                # Filter the current list of connections by each search term
+                connections=$(echo "$connections" | grep -E -i "$i")
         done
 fi
 
