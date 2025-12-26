f99ce6edd1cbc05e01ba4b832c1f12978be93cff
Gustav Abrahamsson <gustav.a.abrahamsson@gmail.com>
2025-12-26 16:13:19 +0100

    Remove need for temp file when searching

--- connections.sh
+++ connections.sh
@@ -1,46 +1,43 @@
 #!/bin/bash
-tmp=~/connect/connections.$$
-cp ~/connect/connections $tmp
+
+# Load initial list
+connections=$(cat ~/connect/connections)
+
 if [ $# -gt 0 ]; then
         search="$*"
-        for i in `echo $search`; do
-                connections=`grep -i $i $tmp >  $tmp.swap`
-                mv $tmp.swap $tmp
+        for i in $search; do
+                # Filter the current list of connections by each search term
+                connections=$(echo "$connections" | grep -E -i "$i")
         done
 fi
 
-connections=`sort $tmp`
+connections=$(echo "$connections" | sort)
 
-if [ `echo $connections | wc -w` -gt 1 ]; then
+if [ $(echo "$connections" | wc -w) -gt 1 ]; then
         # More than one hit, so user has to choose
         select connection in $connections; 
         do
                 echo "$connection"
                 break
         done
-        user=`echo $connection | awk -F@ '{ print $1 }'`
-        host=`echo $connection | awk -F@ '{ print $2 }'`
+        user=$(echo "$connection" | awk -F@ '{ print $1 }')
+        host=$(echo "$connection" | awk -F@ '{ print $2 }')
         hit="true"
-elif [ `echo $connections | wc -w` -eq 1 ]; then
-        # echo " One hit only, no need to choose anything"
-        user=`echo $connections | awk -F@ '{ print $1 }'`
-        host=`echo $connections | awk -F@ '{ print $2 }'`
+elif [ $(echo "$connections" | wc -w) -eq 1 ]; then
+        # One hit only, no need to choose anything
+        user=$(echo "$connections" | awk -F@ '{ print $1 }')
+        host=$(echo "$connections" | awk -F@ '{ print $2 }')
         echo "$user@$host"
         hit="true"
 else
         /usr/bin/host $1 >/dev/null
         if [ $? -eq 0 ]; then
-                # echo " Assume TE host"
-                rm $tmp
                 ~/connect/ssh.exp $1 root
-                # ssh root@
                 exit 0
         else
                 echo "no hit!"
-                rm $tmp
                 exit 1
         fi
 fi
 
-rm $tmp
 ssh -X $user@$host
